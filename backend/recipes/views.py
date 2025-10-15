7c7a5d6aa5c4bcaacc0f09ff31f0336e6980f1b1
Natalya Khusnutdinova <easymatika@yandex.ru>
2025-10-15 10:43:00 +0300

    Исправлен редирект по короткой ссылке.

--- backend/recipes/views.py
+++ backend/recipes/views.py
@@ -4,7 +4,7 @@ from django.urls import reverse
 from .models import Recipe
 
 
-def short_link_redirect(request, pk):
+def short_link_redirect(request, id):
     """Редирект по короткой ссылке."""
-    recipe = get_object_or_404(Recipe, pk=pk)
+    recipe = get_object_or_404(Recipe, id=id)
     return redirect(reverse('recipes-detail', kwargs={'id': recipe.id}))
