930a513131c398ed9c60d14c0c03939d14bce115
Natalya Khusnutdinova <easymatika@yandex.ru>
2025-12-03 20:14:13 +0300

    Build and push for multi-arch

--- backend/recipes/views.py
+++ backend/recipes/views.py
@@ -1,9 +1,4 @@
-from rest_framework.decorators import action
-from rest_framework.response import Response
-from django.shortcuts import get_object_or_404, redirect
-from django.urls import reverse
-
-from .models import Recipe
+from django.shortcuts import redirect
 
 
 def short_link_redirect(request, pk):
